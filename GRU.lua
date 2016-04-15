170e90d3010339f2d57f42a617ba863eae7b9599
SeanNaren <taz838@hotmail.co.uk>
2016-04-15 18:47:13 +0100

    Removed direction set

--- GRU.lua
+++ GRU.lua
@@ -2,7 +2,6 @@ local GRU, parent = torch.class('cudnn.GRU', 'cudnn.RNN')
 
 function BLSTM:__init(inputSize, hiddenSize, numLayers)
     parent.__init(self,inputSize, hiddenSize, numLayers)
-    self.bidirectional = 'CUDNN_UNIDIRECTIONAL'
     self.mode = 'CUDNN_GRU'
     self:reset()
 end
--- LSTM.lua
+++ LSTM.lua
@@ -2,7 +2,6 @@ local BLSTM, parent = torch.class('cudnn.LSTM', 'cudnn.RNN')
 
 function BLSTM:__init(inputSize, hiddenSize, numLayers)
     parent.__init(self,inputSize, hiddenSize, numLayers)
-    self.bidirectional = 'CUDNN_UNIDIRECTIONAL'
     self.mode = 'CUDNN_LSTM'
     self:reset()
 end
