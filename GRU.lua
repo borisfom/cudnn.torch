589fdfd5552c0e9a7fe7b4192fc24de280b3bfc2
SeanNaren <taz838@hotmail.co.uk>
2016-04-15 18:48:15 +0100

    Fixed module names

--- GRU.lua
+++ GRU.lua
@@ -1,6 +1,6 @@
 local GRU, parent = torch.class('cudnn.GRU', 'cudnn.RNN')
 
-function BLSTM:__init(inputSize, hiddenSize, numLayers)
+function GRU:__init(inputSize, hiddenSize, numLayers)
     parent.__init(self,inputSize, hiddenSize, numLayers)
     self.mode = 'CUDNN_GRU'
     self:reset()
--- LSTM.lua
+++ LSTM.lua
@@ -1,6 +1,6 @@
-local BLSTM, parent = torch.class('cudnn.LSTM', 'cudnn.RNN')
+local LSTM, parent = torch.class('cudnn.LSTM', 'cudnn.RNN')
 
-function BLSTM:__init(inputSize, hiddenSize, numLayers)
+function LSTM:__init(inputSize, hiddenSize, numLayers)
     parent.__init(self,inputSize, hiddenSize, numLayers)
     self.mode = 'CUDNN_LSTM'
     self:reset()
