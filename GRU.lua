79fdab5bbdc045913aa66e521b827c52f4379bbd
SeanNaren <taz838@hotmail.co.uk>
2016-04-18 20:04:05 +0100

    Added batchfirst param to modules

--- GRU.lua
+++ GRU.lua
@@ -1,7 +1,7 @@
 local GRU, parent = torch.class('cudnn.GRU', 'cudnn.RNN')
 
-function GRU:__init(inputSize, hiddenSize, numLayers)
-    parent.__init(self,inputSize, hiddenSize, numLayers)
+function GRU:__init(inputSize, hiddenSize, numLayers, batchFirst)
+    parent.__init(self,inputSize, hiddenSize, numLayers, batchFirst)
     self.mode = 'CUDNN_GRU'
     self:reset()
 end
--- LSTM.lua
+++ LSTM.lua
@@ -1,7 +1,7 @@
 local LSTM, parent = torch.class('cudnn.LSTM', 'cudnn.RNN')
 
-function LSTM:__init(inputSize, hiddenSize, numLayers)
-    parent.__init(self,inputSize, hiddenSize, numLayers)
+function LSTM:__init(inputSize, hiddenSize, numLayers, batchFirst)
+    parent.__init(self,inputSize, hiddenSize, numLayers, batchFirst)
     self.mode = 'CUDNN_LSTM'
     self:reset()
 end
