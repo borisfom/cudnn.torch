1aeea062b0d734850dcdd4bfccb6cfae6791e9e0
SeanNaren <taz838@hotmail.co.uk>
2016-04-18 20:06:14 +0100

    Added resets to modules

--- RNNReLU.lua
+++ RNNReLU.lua
@@ -3,4 +3,5 @@ local RNNReLU, parent = torch.class('cudnn.RNNReLU', 'cudnn.RNN')
 function RNNReLU:__init(inputSize, hiddenSize, numLayers, batchFirst)
     parent.__init(self,inputSize, hiddenSize, numLayers, batchFirst)
     self.mode = 'CUDNN_RNN_RELU'
+    self:reset()
 end
